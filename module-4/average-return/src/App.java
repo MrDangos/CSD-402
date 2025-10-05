dddf6f5998d0a57e759e9834b46d18b3c3688316
Hugo Ramirez <hrr4114715@gmail.com>
2025-10-05 03:27:24 -0500

    finished M4 project and added comments on each of the py files

--- module-4/average-return/src/App.java
+++ module-4/average-return/src/App.java
@@ -1,5 +1,53 @@
+/**
+ * M4 AVERAGE ARRAY CALCULATOR
+ * Displays the average of different arrays using methods and AverageCalculator class
+ * @author Hugo Ramirez Jr
+ * @date 10/05/2025
+ */
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        short[] shortArray = {10, 20, 30, 40, 50};
+        int[] intArray = {15, 25, 35, 45};
+        long[] longArray = {100L, 200L, 300L, 400L, 500L};
+        double[] doubleArray = {5.5, 10.5, 15.5, 20.5, 25.5, 30.5};
+
+        System.out.println("----- AVERAGE ARRAY CALCULATOR -----\n");
+
+        System.out.print("Short Array: ");
+        displayArray(shortArray);
+        System.out.println("Average: " + AverageCalculator.average(shortArray) + "\n"); // calculate and display avg
+
+        System.out.print("Int Array: ");
+        displayArray(intArray);
+        System.out.println("Average: " + AverageCalculator.average(intArray) + "\n");
+
+        System.out.print("Long Array: ");
+        displayArray(longArray);
+        System.out.println("Average: " + AverageCalculator.average(longArray) + "\n");
+
+        System.out.print("Double Array: ");
+        displayArray(doubleArray);
+        System.out.println("Average: " + AverageCalculator.average(doubleArray) + "\n");
+    }
+
+    // Displaying arrays
+    public static void displayArray(short[] array) {
+        for (short num : array) System.out.print(num + " ");
+        System.out.println();
+    }
+
+    public static void displayArray(int[] array) {
+        for (int num : array) System.out.print(num + " ");
+        System.out.println();
+    }
+
+    public static void displayArray(long[] array) {
+        for (long num : array) System.out.print(num + " ");
+        System.out.println();
+    }
+
+    public static void displayArray(double[] array) {
+        for (double num : array) System.out.print(num + " ");
+        System.out.println();
     }
 }
---
+++ module-4/average-return/src/AverageCalculator.java
@@ -0,0 +1,43 @@
+public class AverageCalculator {
+/**
+ * Calculates the average of the arrays
+ * @author Hugo Ramirez Jr
+ * @date 10/05/2025
+ */
+    // Calculate average for arrays
+    // short array avg
+    public static short average(short[] array) {
+        int sum = 0;
+        for (short num : array) {
+            sum += num;
+        }
+        return (short) (sum / array.length);
+    }
+
+    // int array avg
+    public static int average(int[] array) {
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        return sum / array.length;
+    }
+
+    // long array avg
+    public static long average(long[] array) {
+        long sum = 0;
+        for (long num : array) {
+            sum += num;
+        }
+        return sum / array.length;
+    }
+
+    // double array avg
+    public static double average(double[] array) {
+        double sum = 0;
+        for (double num : array) {
+            sum += num;
+        }
+        return sum / array.length;
+    }
+}
