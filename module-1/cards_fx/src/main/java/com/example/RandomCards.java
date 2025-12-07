722c9bd75bbfff0c040b799ae59575652cd0b83f
Hugo Ramirez <hrr4114715@gmail.com>
2025-12-07 00:00:59 -0600

    finished project

---
+++ module-1/cards_fx/src/main/java/com/example/RandomCards.java
@@ -0,0 +1,60 @@
+package com.example;
+import java.util.Random;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
+
+public class RandomCards extends Application {
+
+  private Random random = new Random();
+  private HBox cardPane = new HBox(10);
+
+  @Override
+  public void start(Stage primaryStage) {
+    cardPane.setPadding(new Insets(5, 5, 5, 5));
+    cardPane.setAlignment(Pos.CENTER); 
+
+    // Initial draw
+    drawCards();
+
+    // Create Refresh button
+    Button refreshButton = new Button("Refresh");
+    refreshButton.setOnAction(e -> drawCards());
+
+    // Put button in an HBox to center it
+    HBox buttonBox = new HBox(refreshButton);
+    buttonBox.setAlignment(Pos.CENTER);
+    buttonBox.setPadding(new Insets(10));
+
+    // Use BorderPane to arrange layout
+    BorderPane root = new BorderPane();
+    root.setCenter(cardPane);   // cards in the middle
+    root.setBottom(buttonBox);  // button at bottom center
+
+    // Create Scene, place in Stage
+    Scene scene = new Scene(root, 400, 300);
+    primaryStage.setTitle("Random Cards");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  private void drawCards() {
+    cardPane.getChildren().clear(); // remove old cards
+    for (int i = 0; i < 4; i++) {
+      int cardNumber = random.nextInt(52) + 1; 
+      ImageView cardView = new ImageView(new Image("cards/" + cardNumber + ".png"));
+      cardPane.getChildren().add(cardView);
+    }
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
